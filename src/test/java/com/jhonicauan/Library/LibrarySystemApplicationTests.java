6bdcf20e8de20b733e3f295d1b4847cf8f22c147
Jhoni Cauan Freitas Elias <jhonielias8@gmail.com>
2025-08-05 15:57:08 -0300

    Base feita

---
+++ src/test/java/com/jhonicauan/Library/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonicauan.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
